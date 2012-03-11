27b38752b02a226abd82ea8bfba81107258b29e9
Jacob Gold <Jacob@JacobLaptop.(none)>
2012-03-10 20:54:19 -0500

    second commit

---
+++ DivisibleFinder.java
@@ -0,0 +1,36 @@
+
+public class DivisibleFinder {
+	
+	private class List{
+		
+		private class Node{
+			
+			private int data;
+			private Node next = null;
+			
+			private Node(int i){
+				data = i;
+			}
+			
+		}
+		
+		private Node head;
+		
+		private List(){
+			head = null;
+		}
+		
+		private List(int i){
+			head = new Node(i);
+		}
+		
+		private void insert(int i){
+			Node n = head;
+			while(n != null){
+				
+			}
+		}
+		
+	}
+
+}
