6a7eb381995510f8a4d5b8b8f846bbd5f195c391
Jacob Gold <Jacob@JacobLaptop.(none)>
2012-03-12 12:38:27 -0400

    finished and commented

--- DivisibleFinder.java
+++ DivisibleFinder.java
@@ -1,5 +1,13 @@
 
+/*
+ * Goal: What is the smallest number that is evenly divisible by all of the numbers 1-20?
+ * 
+ * http://projecteuler.net/problem=5
+ */
+
 public class DivisibleFinder {
+	
+	//A simple implementation of a node to be used in a list of ints
 	
 	private class Node{
 		
@@ -18,6 +26,8 @@ public class DivisibleFinder {
 		
 	}
 	
+	//A simple list performing only the tasks required by this problem
+	
 	private class List{
 		
 		private Node head;
@@ -30,38 +40,29 @@ public class DivisibleFinder {
 			head = new Node(i);
 		}
 		
-		private List(Node n){
-			head = n;
-		}
+		//Simply inserts at the front of the list as order is not important
 		
 		private void insert(int i){
 			if(head == null){
 				head = new Node(i);
-				return;
 			}
-			if(i < head.data){
+			else{
 				head = new Node(i, head);
-				return;
 			}
-			Node n = head;
-			while(n.next != null){
-				if(i < n.next.data){
-					n.next = new Node(i, n.next);
-					return;
-				}
-				n = n.next;
-			}
-			n.next = new Node(i);
-		}
-		
-		private List next(){
-			return new List(head.next);
 		}
 		
 	}
 	
 	public int findMinDivisible(int maxDivisor){
-		List divisors = new List();
+		List divisors = new List(1);
+		
+		/*
+		 * Each iteration of the for loop checks whether the number is divisible by a divisor i.
+		 * It does this by attempting to divide by the prime factors required to maintain divisibility.
+		 * After the divisor is divided by the prime factors which go evenly into it,
+		 * if the remainder is not 1, it is added as another prime factor.
+		 */
+		
 		for(int i = 1; i <= maxDivisor; i++){
 			Node n = divisors.head;
 			int remainder = i;
@@ -71,8 +72,15 @@ public class DivisibleFinder {
 				}
 				n = n.next;
 			}
-			divisors.insert(remainder);
+			if(remainder != 1){
+				divisors.insert(remainder);
+			}
 		}
+		
+		/*
+		 * The resulting prime factors produced by the above loop are multiplied together
+		 * to create a number divisible by all numbers 1 through maxDivisor.
+		 */
 		
 		Node n = divisors.head;
 		int product = 1;
