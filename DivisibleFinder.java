94117f66530dafdaa3468bf682e7c716ae7d0fce
Jacob Gold <Jacob@JacobLaptop.(none)>
2012-03-12 12:04:10 -0400

    finished

--- DivisibleFinder.java
+++ DivisibleFinder.java
@@ -1,24 +1,24 @@
 
 public class DivisibleFinder {
 	
-	private class List{
+	private class Node{
 		
-		private class Node{
-			
-			private int data;
-			private Node next;
-			
-			private Node(int i){
-				data = i;
-				next = null;
-			}
-			
-			private Node(int i, Node n){
-				data = i;
-				next = n;
-			}
-			
+		private int data;
+		private Node next;
+		
+		private Node(int i){
+			data = i;
+			next = null;
 		}
+		
+		private Node(int i, Node n){
+			data = i;
+			next = n;
+		}
+		
+	}
+	
+	private class List{
 		
 		private Node head;
 		
@@ -28,6 +28,10 @@ public class DivisibleFinder {
 		
 		private List(int i){
 			head = new Node(i);
+		}
+		
+		private List(Node n){
+			head = n;
 		}
 		
 		private void insert(int i){
@@ -45,10 +49,43 @@ public class DivisibleFinder {
 					n.next = new Node(i, n.next);
 					return;
 				}
+				n = n.next;
 			}
 			n.next = new Node(i);
 		}
 		
+		private List next(){
+			return new List(head.next);
+		}
+		
+	}
+	
+	public int findMinDivisible(int maxDivisor){
+		List divisors = new List();
+		for(int i = 1; i <= maxDivisor; i++){
+			Node n = divisors.head;
+			int remainder = i;
+			while(n != null){
+				if(remainder % n.data == 0){
+					remainder = remainder / n.data;
+				}
+				n = n.next;
+			}
+			divisors.insert(remainder);
+		}
+		
+		Node n = divisors.head;
+		int product = 1;
+		while(n != null){
+			product *= n.data;
+			n = n.next;
+		}
+		return product;
+	}
+	
+	public static void main(String[] args){
+		DivisibleFinder df = new DivisibleFinder();
+		System.out.println(df.findMinDivisible(20));
 	}
 
 }
